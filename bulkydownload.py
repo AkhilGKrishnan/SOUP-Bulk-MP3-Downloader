2d609aed85595a147bfe3fec26e202167e5568e7
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2020-07-28 23:55:12 +0530

    custom path selection added

--- bulkydownload.py
+++ bulkydownload.py
@@ -11,25 +11,25 @@ def get_Audio_links():
     r = requests.get(archive_url) 
     # create beautiful-soup object 
     soup = BeautifulSoup(r.content,'html5lib') 
-      
     # find all links on web-page 
     links = soup.findAll('a')
-   
-    directory = soup.title.text
-    directory = directory.replace(" ", "_")
-    print(directory) 
-    os.system('mkdir '+directory+'') 
-  
-    # filter the link sending with .mp4 
-    video_links = [link['href'] for link in links if link['href'].endswith('mp3')] 
     
+    # Folder for storing the downloaded file
+    if args.path:
+        directory = args.path
+    else:    
+        directory = soup.title.text
+        directory = directory.replace(" ", "_")
+        os.system('mkdir '+directory+'') 
+        
+    # filter the link sending with .mp4 
+    file_links = [link['href'] for link in links if link['href'].endswith('mp3')] 
+    return file_links,directory 
   
-    return video_links,directory 
   
+def download_file_series(file_links,directory): 
   
-def download_Audio_series(video_links,directory): 
-  
-    for link in video_links: 
+    for link in file_links: 
         file_name = link.split('/')[-1]    
   
         print("Downloading file:%s"%file_name)
@@ -42,14 +42,16 @@ def download_Audio_series(video_links,directory):
   
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--url", help="for selecting the bulk audio web url")
+parser.add_argument("-u", "--url", help="for choosing the web url for downloadind the files")
+parser.add_argument("-p", "--path", help="folder path for storing the downloaded files")
 args = parser.parse_args()
 if args.url:
     # specify the URL of the archive here 
     archive_url = args.url
     # getting all video links 
-    video_links,directory = get_Audio_links() 
-
+    file_links,directory = get_Audio_links() 
     # download all videos 
-    download_Audio_series(video_links,directory) 
+    download_file_series(file_links,directory) 
+else:
+    print("enter the web url")    
      
